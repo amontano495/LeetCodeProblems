0bfffffdc219068d47c701d16cfde983ed984387
Adam <amontano495@gmail.com>
2022-09-16 09:10:02 -0700

    Time: 58 ms (36.68%), Space: 13.9 MB (12.24%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits) - 1
+        
+        while i >= 0:
+            if digits[i] == 9:
+                digits[i] = 0
+                i -= 1
+            else:
+                digits[i] = digits[i] + 1
+                break
+        if digits[0] == 0:
+            digits.insert(0,1)
+        return digits
+        

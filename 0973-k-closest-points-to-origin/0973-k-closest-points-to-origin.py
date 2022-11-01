6d3c72f41b1200ea8b3ba15e653386fd84aa5ddd
Adam <amontano495@gmail.com>
2022-10-31 18:21:18 -0700

    Time: 1377 ms (63.73%), Space: 19.5 MB (81.59%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -0,0 +1,10 @@
+import math
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        points.sort(key=lambda x: math.sqrt(((x[0] - 0)**2) + ((x[1] - 0)**2)))
+        return points[:k]
